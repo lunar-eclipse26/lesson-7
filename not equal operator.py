d0b2499db54b81f42eb09b76bf05c4074c0f2df5
lunar_eclipse <sleepysmorz@gmail.com>
2025-06-20 20:54:00 +1200

    not equal

---
+++ not equal operator.py
@@ -0,0 +1,18 @@
+a = 20
+b = 50
+c = 50
+print(a != b)
+print(b != c)
+d = "hehe"
+e = "hehehe"
+if a != b:
+    print(a, "and", b, "are not equal")
+f = 1
+g = 7
+if (f == 1) != (g == 3):
+    print("hello")
+a = int(input("enter a number"))
+if a%2 != 0:
+    print(a, "is not an even number")
+else:
+    print(a, "is an even number")
